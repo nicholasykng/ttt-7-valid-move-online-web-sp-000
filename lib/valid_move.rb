b80bfe3816033fcdad85987c978bfb328ca3ac27
Nicholas Ng <nicholasykng@gmail.com>
2019-10-12 17:07:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def valid_move?(board, index)
-  if index.between?(0,8)
-    true
+  if index.between?(0,8) && !position_taken(board, index)
+    return true
 
   end
 end
